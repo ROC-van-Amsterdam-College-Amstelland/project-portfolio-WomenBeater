12e2ba30ca5ed0bb2cc28b5225b930cc64be5230
Henry Benaye <henry.be@outlook.com>
2019-11-05 09:53:31 +0100

    dag 1

---
+++ script.js
@@ -0,0 +1,17 @@
+function Verander1(){
+    document.getElementById("foto1").style.display = "none"
+    document.getElementById("foto2").style.display = "block"
+    document.getElementById("foto3").style.display = "none"
+}
+
+function Verander2(){
+    document.getElementById("foto1").style.display = "none"
+    document.getElementById("foto2").style.display = "none"
+    document.getElementById("foto3").style.display = "block"
+}
+
+function Verander3(){
+    document.getElementById("foto1").style.display = "block"
+    document.getElementById("foto2").style.display = "none"
+    document.getElementById("foto3").style.display = "none"
+}
